b3bbf8d55d28e4f3d936ca014cd64a22aedae27d
natedev14 <nt.domination@gmail.com>
2026-05-06 17:46:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
-  base: '/gissary-stock-editor/',
+  base: '/bling-product-csv-editor/',
   plugins: [react()],
   resolve: {
     alias: {
